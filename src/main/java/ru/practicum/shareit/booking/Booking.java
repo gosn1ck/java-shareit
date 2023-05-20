6e608b9e9d4863c8c5acdb5d58a8890e17d5d62e
Николай Верховцев <gosn1ck@yandex.ru>
2023-05-21 00:09:01 +0300

    feat: add booking fields

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.booking;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
 public class Booking {
+    private Long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Item item;
+    private User user;
+    private Status status;
 }
--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,16 @@
 package ru.practicum.shareit.booking.dto;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingDto {
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Long itemId;
 }
